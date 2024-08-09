061b7fedd9e92d02c60a549812a0aa8c582055c8
isaac <au.to.know.contact@gmail.com>
2024-08-09 13:03:42 +1000

    added spirit death

--- Assets/SpiritScript.cs
+++ Assets/SpiritScript.cs
@@ -11,10 +11,13 @@ public class SpiritScript : MonoBehaviour
     private Vector2 startPosition;
     private bool reverse = false;
     private Rigidbody2D myRB;
+    private Animator myAnimator;
+    private bool alive = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        myAnimator = GetComponent<Animator>();
         myRB = GetComponent<Rigidbody2D>();
         startPosition = transform.position;
         if (Camera.main.transform.position.x < transform.position.x)
@@ -28,9 +31,24 @@ public class SpiritScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float new_x = transform.position.x + horizontalSpeed * Time.deltaTime;
-        float new_y = startPosition.y + (Mathf.Sin(new_x * frequency) * amplitude);
-        myRB.MovePosition(new Vector2(new_x, new_y));
+        if (alive)
+        {
+            float new_x = transform.position.x + horizontalSpeed * Time.deltaTime;
+            float new_y = startPosition.y + (Mathf.Sin(new_x * frequency) * amplitude);
+            myRB.MovePosition(new Vector2(new_x, new_y));
+        } else
+        {
+            float new_x = transform.position.x + horizontalSpeed * Time.deltaTime;
+            float new_y = transform.position.y + horizontalSpeed * Time.deltaTime;
+            myRB.MovePosition(new Vector2(new_x, new_y));
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, GetComponent<SpriteRenderer>().color.a - 25);
+            if (GetComponent<SpriteRenderer>().color.a <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -40,5 +58,14 @@ public class SpiritScript : MonoBehaviour
             //Destroy(gameObject);
         }
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            myAnimator.SetTrigger("death");
+            alive = false;
+        }
     }
 }
